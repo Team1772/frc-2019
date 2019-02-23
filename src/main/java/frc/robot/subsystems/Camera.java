3f7314d6434da3c64b51ef8ec91a8334be53f07d
ezequielmross <ezequielmross@gmail.com>
2019-02-23 15:14:54 -0300

    Fix Pepo changes

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -1,10 +1,17 @@
+package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj.CameraServer;
+
 
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+
 public class Camera{
-    
+
     public void robotInit() {
         CameraServer.getInstance().startAutomaticCapture();
     }
@@ -13,13 +20,13 @@ public class Camera{
         new Thread(() -> {
             UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
             camera.setResolution(640, 480);
-            
+
             CvSink cvSink = CameraServer.getInstance().getVideo();
             CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-            
+
             Mat source = new Mat();
             Mat output = new Mat();
-            
+
             while(!Thread.interrupted()) {
                 cvSink.grabFrame(source);
                 Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
