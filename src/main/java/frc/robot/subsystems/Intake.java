38e98693b42caac978027c4702bbcfeb1f4d8303
Guilherme <agui.82003@gmail.com>
2019-02-23 17:51:40 -0300

    merda

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,27 +3,23 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Solenoid;
-
 // import edu.wpi.first.wpilibj.Encoder;
 
-public class Arm {
+public class Intake {
     // Encoder enc;
-    TalonSRX left, right;
-    
-    public Arm() {
+    TalonSRX movIntake1, movIntake2;
 
-        left = new TalonSRX(0);
-        right = new TalonSRX(3);
+    public Intake() {
+        movIntake1 = new TalonSRX(1);
+        movIntake2 = new TalonSRX(2);
 
         // (Wheel Size centimeter * Math.PI) / Encoder Counts Per Revolution
 		// enc.setDistancePerPulse(distancePerPulse);
     }
 
     public void setSpeed(double speed) {
-        left.set(ControlMode.PercentOutput, speed);
-        right.set(ControlMode.PercentOutput, speed);
+        movIntake1.set(ControlMode.PercentOutput, speed);
+        movIntake2.set(ControlMode.PercentOutput, speed);
         // SmartDashboard.putNumber("Arm encoder", enc.get());
     }
 }
---
+++ src/main/java/frc/robot/subsystems/Pistons.java
@@ -0,0 +1,74 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+// import edu.wpi.first.wpilibj.Encoder;
+
+public class Pistons {
+
+    DoubleSolenoid armLeft, armRight, intake, climbFrontLeft, climbFrontRight, climbBackLeft, climbBackRight; 
+    Compressor comp;
+
+    public Pistons() {
+
+        comp = new Compressor();
+        // armLeft = new DoubleSolenoid(0,1);
+        // armRight = new DoubleSolenoid(0,1);
+        // intake = new DoubleSolenoid(0,1);
+        climbFrontLeft = new DoubleSolenoid(0,1);
+        climbFrontRight = new DoubleSolenoid(0,1);
+        climbBackLeft = new DoubleSolenoid(0,1);
+        climbBackRight = new DoubleSolenoid(0,1);
+
+    }
+
+    public void setClimbFront(boolean activate){
+        if (activate == true){
+            climbFrontRight.set(DoubleSolenoid.Value.kForward);
+            climbFrontLeft.set(DoubleSolenoid.Value.kForward);
+        } else {
+            climbFrontRight.set(DoubleSolenoid.Value.kReverse);
+            climbFrontLeft.set(DoubleSolenoid.Value.kReverse);
+        
+        }
+    }
+
+    public void setClimbBack(boolean activate){
+        if (activate == true){
+            climbBackRight.set(DoubleSolenoid.Value.kForward);
+            climbBackLeft.set(DoubleSolenoid.Value.kForward);
+        } else {
+            climbBackRight.set(DoubleSolenoid.Value.kReverse);
+            climbBackLeft.set(DoubleSolenoid.Value.kReverse);
+        
+        }
+    }
+
+    // public void setArm(boolean activate){
+
+    //     if (activate == true){
+    //         armLeft.set(DoubleSolenoid.Value.kForward);
+    //         armRight.set(DoubleSolenoid.Value.kForward);
+    //     } else {
+    //         armLeft.set(DoubleSolenoid.Value.kReverse);
+    //         armRight.set(DoubleSolenoid.Value.kReverse);
+
+    //     }
+
+    // }
+
+    // public void setIntake(boolean activate){
+    //     if (activate == true){
+    //         intake.set(DoubleSolenoid.Value.kForward);
+
+    //     } else {
+    //         intake.set(DoubleSolenoid.Value.kReverse);
+
+    //     }
+
+    // }
+
+    
+
+}
