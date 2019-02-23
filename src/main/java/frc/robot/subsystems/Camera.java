a52c06b4547081eabcf66d80a33d173892db5965
Pedro Henrique Valentim <pedro.silva@redehost.com.br>
2019-02-23 15:07:44 -0300

    Adicionei classe camera e open CV

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,30 @@
+import edu.wpi.first.wpilibj.CameraServer;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class Camera{
+    
+    public void robotInit() {
+        CameraServer.getInstance().startAutomaticCapture();
+    }
+
+    public void advanced(){
+        new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
+    }
+}
