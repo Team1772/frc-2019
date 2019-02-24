5faa3640a48177250c46a62711982d5bf8f899c8
ezequielmross <ezequielmross@gmail.com>
2019-02-24 14:56:57 -0300

    Improve arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,7 +7,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Arm {
     TalonSRX master, follower;
-    boolean maxLimit;
+    boolean maxLimit, minLimit;
 
     // From core/util/SpeedControl.java
     long currentTime = System.currentTimeMillis(), lastTime = System.currentTimeMillis();
@@ -33,6 +33,7 @@ public class Arm {
         // Safety limit
 		// Fica true se passar do valor maximo ou do minimo
         maxLimit = master.getSelectedSensorPosition(0) > 50000;
+        minLimit = master.getSelectedSensorPosition(0) < 0;
 
         lastPos = master.getSelectedSensorPosition();
     }
@@ -40,7 +41,7 @@ public class Arm {
     // Sempre use essa função para mexer o braco pois ela tem o safety limit
     public void setSpeed(double speed) {
         // Se estiver no limite máximo n deixa o braco se mover
-        if (maxLimit)
+        if (maxLimit && speed > 0 || minLimit && speed < 0)
             speed = 0;
 
             master.set(ControlMode.PercentOutput, speed);
