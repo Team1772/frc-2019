00d2544c5627016ce6531ace206bf99b0b10b83e
Guilherme <agui.82003@gmail.com>
2019-02-24 18:04:20 -0300

    Camera commented

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -7,6 +7,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode.PixelFormat;
 
 public class Camera{
   /*public Camera() {
@@ -20,13 +21,13 @@ public class Camera{
             camera.setResolution(640, 480);
             
             CvSink cvSink = CameraServer.getInstance().getVideo();
-            CvSource outputStream = new CvSource("Blur", PixelFormat.kMJPEG, 640, 480, 30);
+            CvSource outputStream = new CvSource("Blur", PixelFormat.kMJPEG, 640, 480, 10);
             
             Mat source = new Mat();
             Mat output = new Mat();
 
             cvSink.grabFrame(source);
-            Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+            // Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
             outputStream.putFrame(output);
     }
 }
